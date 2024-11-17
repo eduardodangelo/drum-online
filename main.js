48b59aad284db6247dbf83b10ab1027b39ff3d4e
eduardodangelo <html5news@gmail.com>
2024-11-16 22:34:04 -0500

    Added basic files

---
+++ main.js
@@ -0,0 +1,22 @@
+//play sound function
+const playSound = (event) => {
+  const audio = document.querySelector(`audio[data-key = "${event.keyCode}"]`);
+
+  if(!audio) return;
+
+  const key = document.querySelector(`.key[data-key = "${event.keyCode}"]`);
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+};
+
+// remove the css class playing
+const removePlaying = (e) => {
+  if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+};
+
+//select the keys
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removePlaying));
+window.addEventListener('keydown', playSound);
