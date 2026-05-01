ff0a24d8bced41b96175a305054b4805cfcc5b7c
eduardodangelo <html5news@gmail.com>
2026-04-30 20:29:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,3 +21,5 @@ const removePlaying = (e) => {
 const keys = document.querySelectorAll(".key");
 keys.forEach(key => key.addEventListener('transitionend', removePlaying));
 window.addEventListener('keydown', playSound);
+
+<iframe width="990" height="557" src="https://www.youtube.com/embed/1ZYbU82GVz4?list=PLCCV2dadoqCPPWT_zk61DzeegKcGqip5K" title="Flying: Relaxing Sleep Music for Meditation, Stress Relief &amp; Relaxation by Peder B. Helland" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
